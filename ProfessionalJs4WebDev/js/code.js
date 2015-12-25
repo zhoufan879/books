0f90ffa44a007aa0c7ee1f40642d7f98824c2390
zhoufan879 <zhoufan879@zhoufan879deMacBook-Pro.local>
2015-12-25 21:52:37 +0800

    原型概念

---
+++ ProfessionalJs4WebDev/js/code.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+	var divCode = document.createElement("div");
+	var _code = document.createElement("code");
+	divCode.style.margin = '10px';
+	divCode.style.borderWidth = '1px';
+	divCode.style.borderColor = '#ccc';
+	divCode.style.borderStyle = 'solid';
+	divCode.appendChild(_code);
+	
+	var _p = document.createElement("p");
+	_p.style.color = "#eb7350";
+	_p.textContent = '* 请查看控制台 （win - F12 / mac - opt+com+i）';
+	_code.innerText = document.getElementById("js").innerText;
+	
+	document.body.appendChild(divCode);
+	document.body.appendChild(_p);
+}
